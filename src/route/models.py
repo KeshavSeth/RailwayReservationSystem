af4425e1c4216214b73e1dd5b90f93f21c075a2e
mercenary7 <webhead561@gmail.com>
2015-04-27 23:50:41 +0530

    route sorted

--- src/route/models.py
+++ src/route/models.py
@@ -5,35 +5,56 @@ from datetime import datetime
 
 
 class Station(models.Model):
-	name = models.CharField(_("Station Name"),max_length=255)
+    name = models.CharField(_("Station Name"), max_length=255)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
+
 
 class StationSchedule(models.Model):
-	station = models.ForeignKey(Station)
-	arrival = models.TimeField(_("Arrival Time"))
-	departure = models.TimeField(_("Departure Time"))
+    station = models.ForeignKey(Station)
+    arrival = models.DateTimeField(_("Arrival Time"), default=datetime.now())
+    departure = models.DateTimeField(
+        _("Departure Time"), default=datetime.now())
 
-	def __str__(self):
-		return str(self.station) + " | " + str(self.arrival) + " | " + str(self.departure)
+    def __str__(self):
+        return str(self.station) + " | " + str(self.arrival) + " | " + str(self.departure)
 
 
 class RouteManager(models.Manager):
-	def get_schedule(self,Id):
-		schedule = {}
-		schedule['arrival'] = Route.objects.values('schedule__arrival').filter(id =Id)
-		schedule['departure'] = Route.objects.values('schedule__departure').filter(id =Id)
-		schedule['station'] = Route.objects.values('schedule__station').filter(id =Id)
-		return schedule
 
-	def get_train_id(self,route_id):
-		return Route.objects.values().get(id=route_id)['train_id']
+    def get_schedule(self, Id):
+        schedule = {}
+        schedule['arrival'] = Route.objects.values(
+            'schedule__arrival').filter(id=Id)
+        schedule['departure'] = Route.objects.values(
+            'schedule__departure').filter(id=Id)
+        schedule['station'] = Route.objects.values(
+            'schedule__station').filter(id=Id)
+        return schedule
+
+    def get_train_id(self, route_id):
+        return Route.objects.values().get(id=route_id)['train_id']
+
+    def sort_by_arrival(self, schedule):
+        # return sorted(Route.objects.get_schedule(route_id)['arrival'],
+        # key=lambda k: k['schedule__arrival'])
+        x = (schedule)['arrival']
+        y = (schedule)['station']
+        z = (schedule)['departure']
+        indices = sorted(range(len(x)), key=lambda k: x[k])
+        arrival = [x[i] for i in indices]
+        station = [y[i] for i in indices]
+        departure = [z[i] for i in indices]
+        return zip(arrival, departure, station)
+
+
 class Route(models.Model):
-    date = models.DateField(_("Date of journey"), default=datetime.date(datetime.now()))
+    datetime = models.DateTimeField(
+        _("Date of journey"), default=datetime.now())
     train = models.OneToOneField(Train)
     schedule = models.ManyToManyField(StationSchedule, related_name="stations")
     objects = RouteManager()
 
     def __str__(self):
-    	return str(self.train) + " | " + str(self.date)
+        return str(self.train) + " | " + str(self.datetime)
